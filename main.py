2602093079c2415e297d3c2e993ad2984210a945
enzogzz <enzotj.glz@gmail.com>
2021-12-02 09:08:00 +0100

    Linear regression predict

--- main.py
+++ main.py
@@ -5,23 +5,30 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 df = pd.read_pickle("allData.pkl")
-df_test = df[:10]
+df_test = df[:100]
 
 x = df_test['Timestamp']
 y= df_test['Open']
-# plt.scatter(x, y)
-# plt.show()
+
 X = x[:, np.newaxis]
-print(X)
 model = LinearRegression(fit_intercept=True)
 model.fit(X, y)
+
 a = model.coef_
 print("a = ", a[0])
 b = model.intercept_
 print("b = ", b)
 
-xnew = np.array([1325600520])
+xnew = np.array(x)
 ynew = model.predict(xnew.reshape(-1,1))
-print(ynew)
 
-print(df_test)
+plt.scatter(x, y, color='k')
+plt.scatter(xnew, np.zeros(xnew.shape[0]), color='b')
+plt.scatter(xnew, ynew, color='r')
+plt.plot(xnew, ynew, color='m')
+
+ypred = model.predict(X)
+print(ypred.shape)
+print('Erreur quadratique moyenne = ', np.mean((y-ypred)**2))
+
+plt.show()
