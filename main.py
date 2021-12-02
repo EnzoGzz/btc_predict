45998119ff8f616122092bdf36d4c77568ac66d6
enzogzz <enzotj.glz@gmail.com>
2021-12-02 08:48:08 +0100

    Linear regression

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import numpy as np
 import pandas as pd
 import sklearn as sk
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 df = pd.read_pickle("allData.pkl")
+df_test = df[:10]
 
-print(df)
+x = df_test['Timestamp']
+y= df_test['Open']
+# plt.scatter(x, y)
+# plt.show()
+X = x[:, np.newaxis]
+print(X)
+model = LinearRegression(fit_intercept=True)
+model.fit(X, y)
+a = model.coef_
+print("a = ", a[0])
+b = model.intercept_
+print("b = ", b)
+
+# print(df_test)
