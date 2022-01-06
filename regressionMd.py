729fc6a0469c6262d34dab9e9e76451b358050a6
enzogzz <enzotj.glz@gmail.com>
2022-01-06 15:08:44 +0100

    RNN

--- regressionMd.py
+++ regressionMd.py
@@ -4,8 +4,13 @@ import sklearn as sk
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+# import pickle5 as pickle
+
 
 # reading
+
+# with open("allData.pkl", "rb") as fh:  # for python < 3.8
+#   bitcoin = pickle.load(fh)
 
 bitcoin = pd.read_pickle("allData.pkl")
 
@@ -28,15 +33,16 @@ print(model.score(x_test,y_test))
 
 # predicting
 
-future_set = bitcoin.shift(periods=30).tail(30)
+future_set = bitcoin.tail(30)
 
 prediction = model.predict(future_set[required_features])
 
 plt.figure(figsize = (12, 7))
-plt.plot(bitcoin["Timestamp"][-400:-60], bitcoin["Weighted_Price"][-400:-60], color='goldenrod', lw=2)
+plt.plot(bitcoin["Timestamp"][-100:-1], bitcoin["Close"][-100:-1], color='goldenrod', lw=2)
 plt.plot(future_set["Timestamp"], prediction, color='deeppink', lw=2)
 plt.title("Bitcoin Price over time", size=25)
 plt.xlabel("Time", size=20)
 plt.ylabel("$ Price", size=20)
+
 
 plt.show()
